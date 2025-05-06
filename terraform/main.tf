63307332fc339f301a8b85000a0cf571571f6011
murali-fullstackdeveloper <swatha.manoharan@bootlabstech.com>
2025-05-06 19:53:53 +0530

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "aws_route_table" "rtable" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    #cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
